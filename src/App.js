9c8834e09e436e2639d3aaf568c9cc875cfab976
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-09-26 19:44:36 +0200

    Added basic React component structure with examples of conditional rendering, working with arrays, and inline/CSS styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // let name = "Oksana";
+
+    // function getName() {
+    //     return "My name is "
+    // }
+
+    // function getTestText() {
+    //     return console.log("Some test text");
+    // }
+
+    // return (
+    //     <>
+    //     {getName()} {name};
+    //     <p></p>
+    //     <button onClick={getTestText}>Get some text</button>
+    //     </>
+    // );
+
+    {/* Work with arrays */}
+    // const students = [
+    //     { name: "Alex", surname: "Dou", phone: "06604567893" },
+    //     { name: "Maria", surname: "Smith", phone: "06712345678" },
+    //     { name: "John", surname: "Doe", phone: "06898765432" },
+    //     { name: "Anna", surname: "Taylor", phone: "06623456789" },
+    //     { name: "Mark", surname: "Johnson", phone: "06734567890" }
+    // ];
+
+    // return (
+    //     <ul>
+    //         {students.map((element, index) => <li key={index}> {element.name} {element.surname} {element.phone}</li>)}
+    //     </ul>
+    // )
+
+    {/* CSS styles inside jsx */}
+
+    // let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisi nec arcu sodales placerat sed ut mi. Proin sagittis scelerisque orci a elementum. Pellentesque ultricies velit a tortor vestibulum, quis pulvinar sapien aliquet. Sed justo ex, ultricies ut sapien in, dictum dictum orci. Maecenas purus arcu, tristique rhoncus tincidunt sed, laoreet vitae leo. Maecenas et sem sit amet sem cursus mollis non ut arcu. Aliquam eleifend sem tellus. Curabitur vehicula lacinia aliquet. Maecenas ullamcorper, tellus id varius tempus, dui mauris convallis felis, non ultricies purus lacus vel eros.";
+    
+    // const myStyle = {
+    //     'fontWeight': 'bold',
+    //     'fontSize': '20px',
+    //     'color': 'green',
+    //     'padding': '20px'
+    // }
+
+    // const violetTextStyle = 'violet-text';
+
+    // return (
+    //     <>
+    //         <p style={{color: 'blue', fontSize: '18px', fontStyle: 'italic', padding: '20px'}}>{longText}</p>
+    //         <p style={myStyle}>{longText}</p>
+    //         <p className={violetTextStyle}>{longText}</p>
+    //     </>
+    // )
+
+    {/* This component conditionally renders a <h1> element with "Hello World!" if the variable 'x' equals 0 */}
+
+    let x = 0
+
+    return (
+        <>
+            { x === 0 &&
+                (<h1>Hello World!</h1>)
+            }
+        </>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,30 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+{/* Test function to get date and time on click */}
+
+// getDateAndTime(); 
+
+// function getDateAndTime() {
+//     root.render(
+//         <div>
+//             <h2>Today is</h2>
+//             <h3>{new Date().toLocaleDateString()}</h3>
+//             <h2>Time:</h2>
+//             <h3>{new Date().toLocaleTimeString()}</h3>
+//             <button onClick={getDateAndTime}>Update date and time</button>
+//         </div>
+//    )
+// }
+
+// setInterval(getDateAndTime, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
